c8d296c73236a4c9840714316c9afa8888255c86
Raghav Kanda <raghavkanda8@gmail.com>
2024-01-21 18:24:57 -0500

    Semi Final commit. Including graphs and tables

--- Donaldson Paper/scripts/Paper 1 graphing.R
+++ Donaldson Paper/scripts/Paper 1 graphing.R
@@ -23,12 +23,26 @@ mean_sd_values <- data %>%
 print("Mean and Standard Deviation:")
 print(mean_sd_values)
 
-# Scatterplot
-poll_plot <- ggplot(data, aes(x = BALLOTS_CAST, y = BALLOTS_RECEIVED_BY_VOTERS, color = POLL_RESULT)) +
-  geom_point(shape = 19) +
-  labs(title = "Scatter Plot", x = "Ballots Cast", y = "Ballots Received by Voters") +
-  scale_color_manual(values = c("In Favour" = "blue", "Opposed" = "red"))
-print(poll_plot)
+# Ward-wise Analysis - Bar Chart
+ward_turnout <- data %>%
+  group_by(POLL_CD) %>%
+  summarise(turnout_percentage = mean(BALLOTS_CAST / POTENTIAL_VOTERS) * 100)
+
+ggplot(ward_turnout, aes(x = POLL_CD, y = turnout_percentage)) +
+  geom_bar(stat = "identity", color = "red") +
+  ggtitle("Ward-wise Voter Turnout") +
+  xlab("Ward") +
+  ylab("Turnout Percentage") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+print(ward_turnout)
+
+# Subdivision Placement Impact - Scatter Plot
+scatter_plot_subdivision <- ggplot(data, aes(x = ADDRESS, y = BALLOTS_CAST / POTENTIAL_VOTERS * 100)) +
+  geom_point() +
+  labs(title = "Subdivision Placement Impact on Voter Turnout", x = "Subdivision Placement", y = "Voter Turnout (%)")
+
+print(scatter_plot_subdivision)
 
 # Scatter plot with histograms
 poll_plot_with_histograms <- ggplot(data, aes(x = BALLOTS_CAST, y = BALLOTS_RECEIVED_BY_VOTERS, color = POLL_RESULT)) +
@@ -80,3 +94,28 @@ residuals_vs_cast <- ggplot(residual_data, aes(x = BALLOTS_CAST, y = Residuals))
   labs(title = "Residuals vs. Ballots Cast", x = "Ballots Cast", y = "Residual Values") +
   geom_hline(yintercept = 0, linetype = "dashed", color = "red")
 print(residuals_vs_cast)
+
+
+
+
+# Set up a 2 by 2 layout
+par(mfrow = c(2, 2))
+
+# Plot 1: Histogram of residuals
+hist(residual_data$Residuals, main = "Histogram of Residuals", xlab = "Residual Values")
+
+# Plot 2: Boxplot of residuals
+boxplot(residual_data$Residuals, main = "Boxplot of Residuals", ylab = "Residual Values")
+
+# Plot 3: QQ plot of residuals
+qqnorm(residual_data$Residuals)
+qqline(residual_data$Residuals, col = "red")
+title(main = "Normal Probability Plot of Residuals")
+
+# Plot 4: Residuals vs. Ballots Cast
+plot(residual_data$BALLOTS_CAST, residual_data$Residuals,
+     main = "Residuals vs. Ballots Cast", xlab = "Ballots Cast", ylab = "Residual Values")
+abline(h = 0, col = "red", lty = 2)
+
+# Reset the plotting layout
+par(mfrow = c(1, 1))
